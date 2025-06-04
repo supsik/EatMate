ad89f0b556c8f40929fad25de0b1c5a22b424818
supsik <nillkiggers@test.com>
2025-06-04 22:49:34 +0300

    some tg test

--- plugins/telegram.client.ts
+++ plugins/telegram.client.ts
@@ -1,8 +1,19 @@
 export default defineNuxtPlugin(nuxtApp => {
   if (process.client) {
+    const mainStore = useMainStore()
+
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-web-app.js'
-    script.onload = () => window.Telegram.WebApp.ready()
+
+    script.onload = () => {
+      const tg = window.Telegram?.WebApp
+
+      // if (!tg)
+        // return abortNavigation(createError({ statusCode: 404, statusMessage: 'Not found' }));
+      tg?.ready()
+      mainStore.setUserData(tg?.initDataUnsafe?.user || 'error')
+    }
+
     document.head.appendChild(script)
   }
-});
+})
