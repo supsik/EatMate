a1474f03d66bb6aa9d1abecb601381491d7ed4b1
supsik <nillkiggers@test.com>
2025-06-04 22:45:58 +0300

    some tg test

--- middleware/index.global.js
+++ middleware/index.global.js
@@ -2,9 +2,10 @@
 export default defineNuxtRouteMiddleware(() => {
   if (process.client) {
     const mainStore = useMainStore()
+    window.Telegram?.WebApp.ready();
     const tg = window.Telegram?.WebApp;
 
-    mainStore.setUserData(tg?.initDataUnsafe?.user || 900)
+    mainStore.setUserData(tg)
     
     // if (!tg || !tg.initDataUnsafe?.user)
       // return abortNavigation(createError({ statusCode: 404, statusMessage: 'Not found' }));
